6f2819dbedf50a01e4789c960b2ad6cad264bb99
Matt <bbrasky@gmail.com>
2017-01-25 11:08:20 -0500

    added rabbit class to wildlife

--- Wildlife.vb
+++ Wildlife.vb
@@ -1,3 +1,36 @@
-﻿Public Class Wildlife
+﻿Module Wildlife
+    Class Rabbit
+        Dim rHP As Integer = 30
+        Dim rDamage As Integer = 2
+        Dim rFoodRes As Integer = 10
 
-End Class
+        Public Property HP As Integer
+            Get
+                Return rHP
+            End Get
+            Set(ByVal value As Integer)
+                rHP = value
+            End Set
+        End Property
+
+        Public Property Damage As Integer
+            Get
+                Return rDamage
+            End Get
+            Set(ByVal value As Integer)
+                rDamage = value
+            End Set
+        End Property
+
+        Public Property ResourceValue As Integer
+            Get
+                Return rFoodRes
+            End Get
+            Set(ByVal value As Integer)
+                rFoodRes = value
+            End Set
+        End Property
+
+    End Class
+
+End Module
